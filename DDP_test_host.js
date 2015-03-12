50c5659cb77ecf4bad29364fd1b1da3cb3cb5bac
PolGuixe <pol.guixe@gmail.com>
2015-03-12 17:03:59 +0100

    useraccounts branch created

--- DDP_test_host.js
+++ DDP_test_host.js
@@ -1,3 +1,53 @@
+AccountsTemplates.configure({
+  // Behaviour
+  confirmPassword: true,
+  enablePasswordChange: true,
+  forbidClientAccountCreation: false,
+  overrideLoginErrors: true,
+  sendVerificationEmail: false,
+  lowercaseUsername: false,
+
+  // Appearance
+  showAddRemoveServices: false,
+  showForgotPasswordLink: false,
+  showLabels: true,
+  showPlaceholders: true,
+
+  // Client-side Validation
+  continuousValidation: false,
+  negativeFeedback: false,
+  negativeValidation: true,
+  positiveValidation: true,
+  positiveFeedback: true,
+  showValidating: true,
+
+//  // Privacy Policy and Terms of Use
+//  privacyUrl: 'privacy',
+//  termsUrl: 'terms-of-use',
+
+  // Redirects
+//  homeRoutePath: '/',
+//  redirectTimeout: 4000,
+
+  // Hooks
+//  onLogoutHook: myLogoutFunc,
+//  onSubmitHook: mySubmitFunc,
+
+//  // Texts
+//  texts: {
+//    button: {
+//      signUp: "Register Now!"
+//    },
+//    socialSignUp: "Register",
+//    socialIcons: {
+//      "meteor-developer": "fa fa-rocket"
+//    },
+//    title: {
+//      forgotPwd: "Recover Your Passwod"
+//    },
+//  },
+});
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
